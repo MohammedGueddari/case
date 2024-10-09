a02c152d8c9412acefc003e1d30d14dd3ef3d037
MohammedGueddari <74732467+MohammedGueddari@users.noreply.github.com>
2024-10-09 11:55:30 -0300

    ajuste de teste

--- test_glue_job.py
+++ test_glue_job.py
@@ -1,58 +1,64 @@
 import sys
-import boto3
+import os
+import mock
+import pytest
 import json
-import requests
-from awsglue.transforms import *
+from unittest.mock import MagicMock
+from pyspark.sql import SparkSession
 from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.dynamicframe import Dynamicframe
+from unittest.mock import call
 from datetime import datetime
+import calendar
 
+sys.path.append(os.path.abspath('./'))
+import glue_job
+
+spark = SparkSession.builder.getOrCreate()
 
 sc = SparkContext.getOrCreate()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
+sc.addPyFile('./glue_job')
 
-glue_client = boto3.client("glue")
-lambda_client = boto3.client("lambda") 
+@pytest.fixture(autouse=True)
+def append_sys_args():
+    sys.argv.append('--JOB_NAME')
+    yield
+    sys.argv.remove('--JOB_NAME')
 
-def captura_api():
-    url = 'https://api.openbrewerydb.org/breweries'
-    response = requests.get(url)
 
-    df = spark.createDataFrame(response)
-    dynamic_frame = DynamicFrame.fromDF(df, glueContext, "dynamic_frame")
-    return dynamic_frame
-    
+@mock('glue_job.DynamicFrame')
+@mock('glue_job.createDataFrame')
+@mock('glue_job.requests')
+def test_captura_api(
+        mock_requests,
+        mock_createDataFrame,
+        mock_DynamicFrame
+):
+    mock_response_api = MagicMock()
+    atributos = {
+        "get.return_value" : mock_response_api
+    }
+    mock_requests.configure_mock(**atributos)
 
-def grava_resultado(glueContext, dynamicframe):
-    sink = glueContext.getSink(
-        path="s3://bucket/tabelaXX",
-        connection_type="s3",
-        updateBehavior="UPDATE_IN_DATABASE",
-        partitionKeys=[],
-        enableUpdateCatalog=True,
-        transformation_ctx="sink"
-    )
-    sink.setFormat("json")
-    sink.setCatalogInfo(catalogDatabase="target_db_name", catalogTableName="target_table_name")
-    sink.writeFrame(dynamicframe)
+    mock_df = MagicMock()
+    atributos = {
+        "createDataFrame.return_value" : mock_df
+    }
+    mock_createDataFrame.configure_mock(**atributos)
 
-def main():
-    try:
-        grava_resultado(captura_api())
-        glueContext.commit()
-    except:
-        payload = {
-            "job_name": "glue_job)",
-            "key2": datetime.now()
-        }
-        json_payload = json.dumps(payload)
-        response = lambda_client.invoke(
-        FunctionName="function_error",
-        InvocationType='RequestResponse',  # Use 'Event' for async invocation
-        Payload=json_payload
-)
+    mock_DynamicFrame = MagicMock()
+    atributos = {
+        "fromDF.return_value" : mock_DynamicFrame
+    }
+    mock_DynamicFrame.configure_mock(**atributos)
 
-if __name__ == "__main__":
-    main()
+    glue_job.captura_api()
+
+    call_mock_requests = mock_requests.mock_calls
+
+    assert call_mock_requests == mock_response_api
+
+    call_createDataFrame = mock_createDataFrame.mock_calls
+    assert call_createDataFrame == mock_df
+
+    call_DynamicFrame = mock_DynamicFrame.mock_calls
+    assert call_DynamicFrame == mock_DynamicFrame
